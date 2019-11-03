d6861a78b408fbd39a2f8b8a87ebdaa87dda31ad
Viszt Péter <viszt@nold.io>
2019-11-03 20:16:42 +0100

    v0

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+/*
+Plugin Name: VP WooCommerce Áremelés
+Plugin URI: http://visztpeter.me
+Description: Termék árának emelése X százalékkal
+Author: Viszt Péter
+Version: 1.0
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class VP_WC_Increase_Price_In_Bulk {
+  protected static $_instance = null;
+  protected static $background_generator;
+
+	//Get main instance
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+  //Construct
+	public function __construct() {
+
+    //Plugin loaded
+		add_action( 'plugins_loaded', array( $this, 'init' ) );
+
+  }
+
+  public function init() {
+    add_action('admin_menu', array( $this, 'create_menu' ));
+
+    //Setup utility
+    require_once( plugin_dir_path( __FILE__ ) . 'class-bg-process.php' );
+		self::$background_generator = new VP_WC_Increase_Price_In_Bulk_BG();
+  }
+
+	//Create submenu in Tools
+	public function create_menu() {
+		$hook = add_submenu_page( 'woocommerce', 'Áremelés', 'Áremelés', 'manage_options', 'vp-wc-increase-price-in-bulk', array( $this, 'generate_page_content' ) );
+    add_action( "load-$hook", array( $this, 'process_page_submit' ) );
+	}
+
+  function generate_page_content() {
+    ?>
+    <div class="wrap">
+
+      <?php if(get_option('_vp_wc_increase_price_in_bulk_running')) :?>
+      <div class="notice notice-success is-dismissible">
+        <p>Áremelés elindult... eltarhat egy darabig. <?php echo get_option('_vp_wc_increase_price_in_bulk_percentage'); ?>%-al emeli az árat.</p>
+      </div>
+      <?php endif; ?>
+
+      <?php if(get_option('_vp_wc_increase_price_in_bulk_finished')) :?>
+      <div class="notice notice-success is-dismissible">
+        <p>Áremelés befejezve.</p>
+      </div>
+      <?php endif; ?>
+
+      <h1>WooCommerce termék árak emelése x százalékkal</h1>
+      <form method="post">
+        <label>Százalék</label><br>
+        <input type="text" name="percentage" value="5"><br>
+    		<?php submit_button( 'Beállítások mentése', 'primary', 'vp_wc_increase_price_in_bulk_save' ); ?>
+  		</form>
+    </div>
+    <?php
+  }
+
+  public function process_page_submit() {
+    if ( ! empty( $_POST['vp_wc_increase_price_in_bulk_save'] ) ) {
+			update_option('_vp_wc_increase_price_in_bulk_percentage', intval($_POST['percentage']) );
+      update_option('_vp_wc_increase_price_in_bulk_finished', false);
+      update_option('_vp_wc_increase_price_in_bulk_running', true);
+
+      self::$background_generator->push_to_queue( array( 'task' => 'update_products' ) );
+  		self::$background_generator->save()->dispatch();
+    }
+  }
+
+}
+
+//Initialize
+VP_WC_Increase_Price_In_Bulk::instance();
